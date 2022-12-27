1900287f8d638c18fda46b77f600fb6d881e484a
Boumlik-Brahim <boumlik01brahim@gmail.com>
2022-12-27 15:12:42 +0100

    add create_db.sql and adit dockerfile for mariadb

---
+++ srcs/requirements/mariadb/conf/create_db.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS ma_base;
+CREATE USER 'bbrahim' IDENTIFIED BY '12345';
+GRANT ALL PRIVILEGES ON ma_base.* TO 'bbrahim'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'root42';
