9ecbad362a3a09e3e97f247d396f47735d0331ba
Boumlik-Brahim <boumlik01brahim@gmail.com>
2022-12-28 16:58:11 +0100

    edit mariadb 50-server.cnf configuration file

--- srcs/requirements/mariadb/conf/create_db.sql
+++ srcs/requirements/mariadb/conf/create_db.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE IF NOT EXISTS ma_base;
-CREATE USER 'bbrahim' IDENTIFIED BY '12345';
+CREATE USER 'bbrahim' IDENTIFIED BY 'user42';
 GRANT ALL PRIVILEGES ON ma_base.* TO 'bbrahim'@'%';
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'root42';
