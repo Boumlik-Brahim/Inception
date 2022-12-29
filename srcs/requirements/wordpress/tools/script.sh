c6254a29cd681af2b1af65b518edc6bd4ed41820
Boumlik-Brahim <boumlik01brahim@gmail.com>
2022-12-29 18:56:21 +0100

    add wordpress dockerfile and script.sh

---
+++ srcs/requirements/wordpress/tools/script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wp core install --url=bbrahim.42.fr \
+                --title="Inception" \
+                --admin_name=bbrahim \
+                --admin_password=admin@42 \
+                --admin_email=bbrahim@student.1337.ma
