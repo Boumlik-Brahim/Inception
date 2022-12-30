4fccad46fc4516e73140b70cc0e37fbfb216ea6d
Boumlik-Brahim <boumlik01brahim@gmail.com>
2022-12-30 17:45:59 +0100

    add wp config create and wp user createtoscript.sh

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -5,3 +5,19 @@ wp core install --url=bbrahim.42.fr \
                 --admin_name=bbrahim \
                 --admin_password=admin@42 \
                 --admin_email=bbrahim@student.1337.ma
+
+wp config create --dbname=ma_base \
+                --dbuser=bbrahim \
+                --dbpass=user42 \
+                --dbhost=localhost \
+                --path=/var/www/html/ \
+                --allow-root
+
+wp user create bbrahim \
+                bbrahim@student.1337.ma \
+                --role=author \
+                --user_pass=wpuser@123 \
+                --allow-root \
+                --path=/var/www/html/
+
+/usr/sbin/php-fpm7.3 --nodaemonize --allow-to-run-as-root
