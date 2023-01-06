2138da0a5e3482cb28769cd13c9a2bb86a38e282
Boumlik-Brahim <boumlik01brahim@gmail.com>
2023-01-06 12:02:58 +0100

    edit ftp dockerfile and vsftpd.conf

--- srcs/requirements/bonus/FTP/tools/script.sh
+++ srcs/requirements/bonus/FTP/tools/script.sh
@@ -14,18 +14,18 @@ echo "vsftpd test file" | tee /home/bbrahim/ftp/files/test.txt
 
 echo "bbrahim" | tee -a /etc/vsftpd.userlist
 
-echo "
-anonymous_enable=NO
-local_enable=YES
-write_enable=YES
-chroot_local_user=YES
-user_sub_token=$bbrahim
-local_root=/home/$bbrahim/ftp
-pasv_min_port=40000
-pasv_max_port=40005
-userlist_enable=YES
-userlist_file=/etc/vsftpd.userlist
-userlist_deny=NO" >> /etc/vsftpd.conf
+# echo "
+# anonymous_enable=NO
+# local_enable=YES
+# write_enable=YES
+# chroot_local_user=YES
+# user_sub_token=bbrahim
+# local_root=/home/bbrahim/ftp
+# pasv_min_port=40000
+# pasv_max_port=40005
+# userlist_enable=YES
+# userlist_file=/etc/vsftpd.userlist
+# userlist_deny=NO" >> /etc/vsftpd.conf
 
 service vsftpd start
 service vsftpd stop
