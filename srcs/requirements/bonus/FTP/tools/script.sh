03618bf9bc830e21e5d62abfa1239785b17fb9ae
Boumlik-Brahim <boumlik01brahim@gmail.com>
2023-01-10 18:31:36 +0100

    Edit Ftp dockerfile and script

--- srcs/requirements/bonus/FTP/tools/script.sh
+++ srcs/requirements/bonus/FTP/tools/script.sh
@@ -14,21 +14,21 @@ chmod a-w /home/ftp_client/ftp
 mkdir -p /home/ftp_client/ftp/files
 chown ftp_client:ftp_client /home/ftp_client/ftp/files
 
-# sed -i -r "s/#write_enable=YES/write_enable=YES/1"   /etc/vsftpd.conf
-# sed -i -r "s/#chroot_local_user=YES/chroot_local_user=YES/1"   /etc/vsftpd.conf
+sed -i -r "s/#write_enable=YES/write_enable=YES/1"   /etc/vsftpd.conf
+sed -i -r "s/#chroot_local_user=YES/chroot_local_user=YES/1"   /etc/vsftpd.conf
 
-# echo "
-# local_enable=YES
-# allow_writeable_chroot=YES
-# pasv_enable=YES
-# local_root=/var/www/html/wordpress
-# pasv_min_port=40000
-# pasv_max_port=40005
-# userlist_enable=YES
-# userlist_file=/etc/vsftpd.userlist
-# userlist_deny=NO" >> /etc/vsftpd.conf
+echo "
+local_enable=YES
+allow_writeable_chroot=YES
+pasv_enable=YES
+local_root=/var/www/html/wordpress
+pasv_min_port=40000
+pasv_max_port=40005
+userlist_enable=YES
+userlist_file=/etc/vsftpd.userlist
+userlist_deny=NO" >> /etc/vsftpd.conf
 
-# service vsftpd start
-# service vsftpd stop
+service vsftpd start
+service vsftpd stop
 
 /usr/sbin/vsftpd
