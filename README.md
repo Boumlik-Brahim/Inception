3cdd0015a3c0a94577ff0a82be2eb3f1f41ccfaf
Boumlik-Brahim <boumlik01brahim@gmail.com>
2023-01-11 18:23:20 +0100

    FTP finished

--- README.md
+++ README.md
@@ -2,31 +2,3 @@
 
 ##  Docker
 
-# echo "vsftpd test file" | tee /home/$ftp_client/ftp/files/test.txt
-
-# echo "
-# user_sub_token=ftp_client
-# local_root=/home/bbrahim/ftp
-# pasv_min_port=40000
-# pasv_max_port=40005
-# userlist_enable=YES
-# userlist_file=/etc/vsftpd.userlist
-# userlist_deny=NO" >> /etc/vsftpd.conf
-
-# set -e
-
-# if ! cat /etc/vsftpd.userlist | grep www-data > /dev/null 2>&1; then
-# 	chsh -s /bin/bash www-data
-# 	usermod -d /var/www/html/wordpress www-data
-# 	echo "www-data:testpass" | chpasswd
-# 	chown -R www-data:www-data /var/www/html/wordpress
-# 	echo www-data | tee -a /etc/vsftpd.userlist > /dev/null 2>&1
-# fi
-
-# service vsftpd start
-# service vsftpd stop
-
-# echo "*****Starting FTP Container*****"
-
-# exec "$@"
-# Footer
--- srcs/requirements/bonus/FTP/tools/script.sh
+++ srcs/requirements/bonus/FTP/tools/script.sh
@@ -1,32 +1,18 @@
 #!/bin/bash
 
-adduser ftp_client --disabled-password
+adduser $FTP_CLIENT --disabled-password
 
-echo "ftp_client:ftp_pass" | /usr/sbin/chpasswd &> /dev/null
+echo "$FTP_CLIENT:$FTP_PASS" | /usr/sbin/chpasswd
 
-echo "ftp_client" | tee -a /etc/vsftpd.userlist &> /dev/null
+echo "$FTP_CLIENT" | tee -a /etc/vsftpd.userlist
 
-mkdir -p /home/ftp_client/ftp
+mkdir -p /home/$FTP_CLIENT/ftp
 
-chown nobody:nogroup /home/ftp_client/ftp
-chmod a-w /home/ftp_client/ftp
+chown nobody:nogroup /home/$FTP_CLIENT/ftp
+chmod a-w /home/$FTP_CLIENT/ftp
 
-mkdir -p /home/ftp_client/ftp/files
-chown ftp_client:ftp_client /home/ftp_client/ftp/files
-
-sed -i -r "s/#write_enable=YES/write_enable=YES/1"   /etc/vsftpd.conf
-sed -i -r "s/#chroot_local_user=YES/chroot_local_user=YES/1"   /etc/vsftpd.conf
-
-echo "
-local_enable=YES
-allow_writeable_chroot=YES
-pasv_enable=YES
-local_root=/var/www/html/wordpress
-pasv_min_port=40000
-pasv_max_port=40005
-userlist_enable=YES
-userlist_file=/etc/vsftpd.userlist
-userlist_deny=NO" >> /etc/vsftpd.conf
+mkdir /home/$FTP_CLIENT/ftp/files
+chown $FTP_CLIENT:$FTP_CLIENT /home/$FTP_CLIENT/ftp/files
 
 service vsftpd start
 service vsftpd stop
